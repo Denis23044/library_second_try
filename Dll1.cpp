995f195ed281964cc51a0a397c2caf0001031fbc
Denis23044 <103368081+Denis23044@users.noreply.github.com>
2022-05-23 09:57:29 +0300

    Add files via upload

---
+++ Dll1.cpp
@@ -0,0 +1,59 @@
+﻿// Dll1.cpp : Определяет экспортируемые функции для DLL.
+//
+
+#include "framework.h"
+#include "Dll1.h"
+
+/*
+// Пример экспортированной переменной
+DLL1_API int nDll1=0;
+
+// Пример экспортированной функции.
+DLL1_API int fnDll1(void)
+{
+    return 0;
+}
+
+// Конструктор для экспортированного класса.
+CDll1::CDll1()
+{
+    return;
+}*/
+extern "C" {
+
+    Plate* create_plate(double x1, double x2, double y1, double y2, int plateNumber) {
+        return (new Plate(x1, x2, y1, y2, plateNumber));
+    }
+    void delete_plate(Plate* plate) {
+        delete plate;
+    }
+    int add_contact_data(Plate* plate, Device* device, int x, int y, int contact_num, int plate_num, const string& name) {
+        ContactData* contact = new ContactData(device, x, y, contact_num, plate_num, name);
+        return plate->addContact(contact);
+    }
+    int add_power_contact(Plate* plate, Device* device, int x, int y, int contact_num, int plate_num, double power) {
+        PowerContact* contact = new PowerContact(device, x, y, contact_num, plate_num, power);
+        return plate->addContact(contact);
+    }
+    int remove_contact(Plate* plate, int x, int y) {
+        //Не смог найти информацию (ну, либо просто не понял), удалит ли Plate.cpp:272
+        // this->contacts.erase(this->contacts.begin()+i);
+        //Лежащий там объект Contact или нет.
+        //Пока оставлю так, если что, нужно будет прописать вручную удаление объекта
+        return plate->removeContact(x, y);
+    }
+    void contact_info(Contact* contact) {
+        contact->print();
+    }
+    void plate_info(Plate* plate) {
+        plate->print();
+    }
+    Device* create_device(string name) {
+        return new Device(name);
+    }
+    void delete_device(Device* device) {
+        delete device;
+    }
+
+
+}
---
+++ Dll1.h
@@ -0,0 +1,39 @@
+﻿// Приведенный ниже блок ifdef — это стандартный метод создания макросов, упрощающий процедуру
+// экспорта из библиотек DLL. Все файлы данной DLL скомпилированы с использованием символа DLL1_EXPORTS
+// Символ, определенный в командной строке. Этот символ не должен быть определен в каком-либо проекте,
+// использующем данную DLL. Благодаря этому любой другой проект, исходные файлы которого включают данный файл, видит
+// функции DLL1_API как импортированные из DLL, тогда как данная DLL видит символы,
+// определяемые данным макросом, как экспортированные.
+#include "../../../task2/Plate.h"
+#include "../../../task2/ContactData.h"
+#include "../../../task2/PowerContact.h"
+
+#ifdef DLL1_EXPORTS
+#define DLL1_API __declspec(dllexport)
+#else
+#define DLL1_API __declspec(dllimport)
+#endif
+/*
+// Этот класс экспортирован из библиотеки DLL
+class DLL1_API CDll1 {
+public:
+	
+	CDll1(void);
+	// TODO: добавьте сюда свои методы.
+};
+
+extern DLL1_API int nDll1;*/
+
+DLL1_API int fnDll1(void);
+extern "C" {
+	Plate* create_plate(double x1 = 0, double x2 = 0, double y1 = 0, double y2 = 0, int plateNumber = 1);
+	void delete_plate(Plate* plate);
+	int add_contact_data(Plate* plate, Device* device, int x = 0, int y = 0, int contact_num = 0, int plate_num = 0, const string& name = "def");
+	int add_power_contact(Plate* plate, Device* device, int x = 0, int y = 0, int contact_num = 0, int plate_num = 0, double power = 0);
+	int remove_contact(Plate* plate, int x, int y);
+	void contact_info(Contact* contact);
+	void plate_info(Plate* plate);
+	Device* create_device(string name);
+	void delete_device(Device* device);
+};
+
